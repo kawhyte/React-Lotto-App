e2a0ae3ccceb30f73413edf4e6dd85f99a1942d8
Kenny <kawhyte@gmail.com>
2019-08-06 12:33:02 -0700

    added favicon and ticket cost

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,12 +5,14 @@ const Card = ({ticketName, topPrize, gameImageLink, remainingPrizes, overallOdds
 
 return (
 <div class="tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5 w5">
+<h2 id={1} class=" tl f5 fw4 gray mt0">${remainingPrizes}</h2>
   <div class="tc">
     <img src={gameImageLink} class="br-50 h4 w4 dib ba b--black-05 pa2" title="Lotto ticket"/>
     <h1 class="f3 mb2 truncate">{ticketName}</h1>
     <hr class="mw4 bb bw1 b--black-10"></hr>
-    <h2 class="f5 fw4 gray mt0">Top prize remaining : {remainingPrizes}</h2>
-    <h2 class="f5 fw4 gray mt0">Odds of winning : {overallOddsOfWinning}</h2>
+    <h2 class="number f5 fw4 gray mt0">Top prize: ${topPrize}</h2>
+    <h2 class="f5 fw4 gray mt0">Top prize remaining: {remainingPrizes}</h2>
+    <h2 class="f5 fw4 gray mt0">Odds of winning: {overallOddsOfWinning}</h2>
   </div>
 </div>
 );
